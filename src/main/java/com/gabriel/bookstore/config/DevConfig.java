a954cfdc8f8f9e57d5828a3af5e67cc17c6ea59d
G4brielMedeiros <GabrielLinsMedeiros@gmail.com>
2021-10-02 00:21:23 -0300

    Dev profile implemented.

---
+++ src/main/java/com/gabriel/bookstore/config/DevConfig.java
@@ -0,0 +1,29 @@
+package com.gabriel.bookstore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.gabriel.bookstore.service.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean buildsDatabase() {
+		if(strategy.equals("create"))
+			this.dbService.buildDatabase();
+		
+		return false;
+	}
+
+}
